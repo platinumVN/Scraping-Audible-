36cf3fca494bc44b49f220d8b1a2064384769a10
thupla <thuleanhphan@gmail.com>
2023-03-16 00:31:20 +0700

    debug MongodbPipeline

--- Scrapy/scraping_audible/scraping_audible/pipelines.py
+++ Scrapy/scraping_audible/scraping_audible/pipelines.py
@@ -13,7 +13,7 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-dotenv_path = Path(f'{Path(__file__).resolve().parents[1]}\env_vars.env')
+dotenv_path = f'{Path(__file__).resolve().parents[4]}' + '\private_env.env'
 load_dotenv(dotenv_path=dotenv_path)
 
 CONNECTION_STRING = os.getenv('MONGODB_CONNECTION_STRING')
--- Scrapy/scraping_audible/scraping_audible/test.py
+++ Scrapy/scraping_audible/scraping_audible/test.py
@@ -4,7 +4,20 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-dotenv_path = Path(f'{Path(__file__).resolve().parents[1]}\env_vars.env')
+dotenv_path = f'{Path(__file__).resolve().parents[4]}' + '\private_env.env'
+# dotenv_path = f
+print(dotenv_path)
+# dotenv_path = r'C:\Users\ANH THU\OneDrive\CODE\scrapers\env.env'
+
 load_dotenv(dotenv_path=dotenv_path)
+# "C:\Users\ANH THU\OneDrive\CODE\scrapers\env_vars.env"
 
 CONNECTION_STRING = os.getenv('MONGODB_CONNECTION_STRING')
+print(CONNECTION_STRING)
+
+import pymongo
+# client = pymongo.MongoClient("mongodb+srv://thuphan:Ll3uxKE8yr6Vecb6@clusterthuscraping.pytlyml.mongodb.net/?retryWrites=true&w=majority")
+# db = client['Audible']
+
+# print(db)
+# print(db.get_collection())
